51e09caae9368e25bdee31028880547bfbef800b
anderson <andreazzabr@gmail.com>
2020-04-26 17:39:07 +1000

    update alb.tf

--- terraform/alb/alb.tf
+++ terraform/alb/alb.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "lb" {
   name               = "${var.projectname}-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.sg.id]
-  subnets            = data.aws_subnet_ids.subs.ids
+  security_groups    = [aws_security_group.ecs-ec2-sg.id]
+  subnets            = aws_subnet.public-subs.ids
   tags = {
     Name = "${var.projectname}-lb"
   }
@@ -21,34 +21,10 @@ resource "aws_lb_target_group" "lbtg" {
   name     = "${var.projectname}-lb-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.vpc.id
+  vpc_id   = aws_vpc.main.id
 }
 resource "aws_lb_target_group_attachment" "lbtga" {
   target_group_arn = aws_lb_target_group.lbtg.arn
-  target_id        = data.aws_instance.inst.id
+  target_id        = aws_ecs_service.project_ecs_service.id
   port             = 80
-}
-
-
-data "aws_instance" "inst" {
-  filter {
-    name   = "image-id"
-    values = [var.ami]
-  }
-}
-data "aws_vpc" "vpc" {
-    filter {
-        name = "tag:Name"
-        values = ["${var.projectname}*"]
-    }
-}
-data "aws_subnet_ids" "subs" {
-    vpc_id = data.aws_vpc.vpc.id
-  filter {
-        name = "tag:Name"
-        values = ["${var.projectname}-public*"]
-  }
-}
-data "aws_security_group" "sg" {
-    vpc_id = data.aws_vpc.vpc.id
 }
