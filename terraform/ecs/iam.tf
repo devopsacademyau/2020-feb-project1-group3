6395b4b10d117d6f3b0bfe091a401cb83d57ae7d
Siyuan Hu <hsy3418@gmail.com>
2020-04-28 11:22:38 +1000

    fix things

--- terraform/ecs/iam.tf
+++ terraform/ecs/iam.tf
@@ -20,6 +20,7 @@ resource "aws_iam_role_policy_attachment" "ecs_node_role_policy" {
   role       = aws_iam_role.ecsrun.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+
 resource "aws_iam_instance_profile" "instprof" {
     name    = "${var.projectname}-inst-prof"
     role    = aws_iam_role.ecsrun.name
