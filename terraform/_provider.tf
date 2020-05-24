1b3fb55a5a6cd130cc587936c59b2506fd0e0ff7
Siyuan Hu <hsy3418@gmail.com>
2020-05-24 19:14:11 +1000

    add terraform lint for testing

--- terraform/_provider.tf
+++ terraform/_provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   version = "~> 2.0"
   region = "ap-southeast-2"
+  access_key = "AKIAY3DKZSVUFTX7SSUM"
+  secert_key = "PSmsBhitbLZcSCBM5DXfQIXyiuLkRSg4tF0qXz+P"
 }
 
 provider "random" {
